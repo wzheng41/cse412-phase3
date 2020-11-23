794401e314d1561b4dfff8cd2c85363fbf31233a
wzheng41 <47991418+wzheng41@users.noreply.github.com>
2020-11-22 18:20:23 -0700

    Update admin.py

--- restaurants/restaurant/admin.py
+++ restaurants/restaurant/admin.py
@@ -3,13 +3,13 @@ from django.contrib import admin
 from restaurant import  models
 
 # Register your models here.
-####################系统相关配置######################
+####################system setting######################
 admin.site.site_header = 'Restaurants'
 admin.site.site_title = 'Restaurants'
 #####################################################
 @admin.register(models.Draft)
 class DraftAdmin(admin.ModelAdmin):
-    list_display = ('permit_id','yelp_id') # 展示的哪些字段
+    list_display = ('permit_id','yelp_id') # show column
     list_display_links = ('permit_id',) #
     search_fields = ('permit_id', 'yelp_id')
     list_per_page = 50
@@ -21,7 +21,7 @@ class DraftAdmin(admin.ModelAdmin):
 class InspectionsAdmin(admin.ModelAdmin):
     list_display = ('inspection','permit_id','date','time','inspection_type','critical_violations',
                     'non_critical_violations','critical_violations_corrected_on_site','critical_violations_to_be_resolved',
-                    'non_critical_violations_corrected_on_site','non_critical_violations_to_be_resolved') # 展示的哪些字段？
+                    'non_critical_violations_corrected_on_site','non_critical_violations_to_be_resolved') # show column
     list_display_links = ('inspection',) #
     search_fields = ('inspection', 'permit_id')
     list_per_page = 50
@@ -31,7 +31,7 @@ class InspectionsAdmin(admin.ModelAdmin):
 
 @admin.register(models.Opentable)
 class OpentableAdmin(admin.ModelAdmin):
-    list_display = ('permit_id','opentableid') # 展示的哪些字段？
+    list_display = ('permit_id','opentableid') # show column
     list_display_links = ('permit_id',) #
     search_fields = ('permit_id', 'opentableid')
     list_per_page = 50
@@ -41,7 +41,7 @@ class OpentableAdmin(admin.ModelAdmin):
 
 @admin.register(models.Restaurants)
 class RestaurantsAdmin(admin.ModelAdmin):
-    list_display = ('permit_id','name','address','lattitude','longitude',) # 展示的哪些字段？
+    list_display = ('permit_id','name','address','lattitude','longitude',) # show column
     list_display_links = ('permit_id',) #
     search_fields = ('permit_id', 'name')
     list_per_page = 50
@@ -51,7 +51,7 @@ class RestaurantsAdmin(admin.ModelAdmin):
 
 @admin.register(models.Violationcodeskey)
 class ViolationcodeskeyAdmin(admin.ModelAdmin):
-    list_display = ('violation_number','violation_description','violation_category',) # 展示的哪些字段？
+    list_display = ('violation_number','violation_description','violation_category',) # show column
     list_display_links = ('violation_number',) #
     search_fields = ('violation_number', 'violation_category')
     list_per_page = 50
@@ -60,7 +60,7 @@ class ViolationcodeskeyAdmin(admin.ModelAdmin):
 
 @admin.register(models.Violations)
 class ViolationsAdmin(admin.ModelAdmin):
-    list_display = ('inspection_id','violation_number','violation_text',) # 展示的哪些字段？
+    list_display = ('inspection_id','violation_number','violation_text',) # show column
     list_display_links = ('inspection_id',) #
     search_fields = ('inspection_id', 'violation_number')
     list_per_page = 50
